f4565c5a8dbe658a0f8e4308005072fbb58d9d3d
Igor Fedkov <igor.fedkov@gmail.com>
2020-08-15 01:10:42 +0300

    Меняет запятые на точки

--- task-6.js
+++ task-6.js
@@ -1,17 +1,16 @@
 let input;
 let total = 0;
-let message;
 
 input = prompt(`Введите число`, 0);
 while (input != null) {
+	input = input.replace(/,/, '.');
 	input = Number(input);
-	if (input == NaN) {
-		message = `Было введено не число, попробуйте еще раз`;
+	if (isNaN(input)) {
+		alert(`Было введено не число, попробуйте еще раз`);
 	} else {
 		total += input;
 	}
 	input = prompt(`Введите число`);
 }
 
-message = `Общая сумма чисел равна ${total}`;
-alert(message);
+alert(`Общая сумма чисел равна ${total}`);
